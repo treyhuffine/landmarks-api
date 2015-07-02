1f7673eed8142b76a30d66249fdd1d7e6d985f25
Trey Huffine <treyhuffine@gmail.com>
2015-07-01 17:19:41 -0700

    Initialize and test fake data for apiv1

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var apiv1 = require('./routes/apiv1')
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/apiv1', apiv1);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
