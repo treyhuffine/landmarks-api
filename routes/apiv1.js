6a98e10a7988b9f61cf8e317fe14b03f92e73263
Trey Huffine <treyhuffine@gmail.com>
2015-07-01 17:32:53 -0700

    Pull data successfully from firebase

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -3,15 +3,10 @@ var Firebase = require('firebase');
 var router = express.Router();
 var fbRef = new Firebase('https://treyhuffine-sample-apps.firebaseio.com/landmarks-app/locations');
 
-var fakeData = [
-  { name: 'location1', description: 'Location 1', year: 1999 },
-  { name: 'location2', description: 'Location 2', year: 1998 },
-  { name: 'location3', description: 'Location 3', year: 1992 },
-  { name: 'location4', description: 'Location 4', year: 1995 }
-];
-
 router.get('/locations', function(req, res, next) {
-  res.json( { locations: fakeData } );
+  fbRef.once("value", function(snap) {
+    res.json( { locations: snap.val() } );
+  });
 });
 router.post('/locations', function(req, res, next) {
   fbRef.push(req.body);
